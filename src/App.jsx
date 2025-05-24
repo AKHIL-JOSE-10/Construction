08450bd9fcca93953ce2044dfabf8db00e8ac23c
AlwinMathew2003 <alwinchungath@gmail.com>
2025-05-24 19:22:08 +0530

    created normal routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { Grid, Typography, Avatar, Box, Button, Accordion, Tabs, AccordionSummary, AccordionDetails, Tab, Paper } from '@mui/material';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+
 import MobHeading from './modules/components/Mobile/mobileHeading';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import VerifiedIcon from '@mui/icons-material/Verified';
-import MobBookingInfo from './pages/Mobile/mobBookingInfo'
+import MobBookingInfo from './pages/Mobile/mobBookingInfo';
 import MobmyBookings from './pages/Mobile/mobmyBookings';
 import MobProfilePage from './pages/Mobile/mobProfile';
 import MobSettings from './pages/Mobile/mobSettings';
 import MobNotification from './pages/Mobile/mobNotification';
 import Signinpage from './pages/signinpage';
-import Home from "./modules/pages/Home/Mobile/Home"
-import Profile from "./modules/pages/Profile/Profile"
+import Home from "./modules/pages/Home/Mobile/Home";
+import Profile from "./modules/pages/Profile/Profile";
 import Mobsignin from './pages/Mobile/Authentication/mobsignin';
 import MobSignup from './pages/Mobile/Authentication/mobSignup';
 import MobServiceCategory from './pages/Mobile/mobServiceCategory';
@@ -18,21 +21,23 @@ import MobForgotPassword from './pages/Mobile/Authentication/mobForgotPassword';
 
 function App() {
   return (
-    // <Signinpage/>
-    // <Signuppage/>
-    // <MobProfilePage/>
-    // <MobmyBookings/>
-    // <MobWorkerpage/>
-  //  <MobBookingInfo/>
-  //  <MobServiceCategory/>
-  // <MobSettings/>
-  // // <MobNotification />
-  // <Home/>
-  // <Profile/>
-  // <Bookings/>
-  // <Mobsignin/>
-  <MobSignup/>
-  // <MobForgotPassword />
+    <Router>
+      <Routes>
+        <Route path="/signin" element={<Signinpage />} />
+        <Route path="/mobile-signin" element={<Mobsignin />} />
+        <Route path="/mobile-signup" element={<MobSignup />} />
+        <Route path="/mobile-profile" element={<MobProfilePage />} />
+        <Route path="/mobile-bookings" element={<MobmyBookings />} />
+        <Route path="/mobile-booking-info" element={<MobBookingInfo />} />
+        <Route path="/mobile-service-category" element={<MobServiceCategory />} />
+        <Route path="/mobile-workerpage" element={<MobWorkerpage />} />
+        <Route path="/mobile-settings" element={<MobSettings />} />
+        <Route path="/mobile-notifications" element={<MobNotification />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/forgot-password" element={<MobForgotPassword />} />
+      </Routes>
+    </Router>
   );
 }
 
