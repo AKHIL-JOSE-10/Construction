d38a33bcd29ee08337e4508a86554caabd6817a1
Akhil-jose-10 <akhiljosetcr@gmail.com>
2025-05-25 17:22:34 +0530

    fixed calender

--- src/modules/components/Mobile/mobWorkerService.jsx
+++ src/modules/components/Mobile/mobWorkerService.jsx
@@ -69,7 +69,7 @@ const MobWorkerservice = () => {
         <Typography sx={{ fontSize: '18px', fontWeight: '600', mt: 4, ml: 2 }}>Availability</Typography>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           <Box sx={{border:'2px solid #E5E5E5', display: 'flex', justifyContent: 'center', mt: 3, mb: 2, px: 2 }}>
-            <Box sx={{ borderRadius: 3, padding: 2, maxWidth: 360, width: '100%' }}>
+            <Box sx={{ borderRadius: 3, maxWidth: 360, width: '100%' }}>
               <StaticDatePicker
                 value={dayjs()}
                 onChange={() => { }}
