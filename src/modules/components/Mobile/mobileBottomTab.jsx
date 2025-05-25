1e895c97c1c32982bb2e62ce2b7c7041bdd0ef74
Akhil-jose-10 <akhiljosetcr@gmail.com>
2025-05-25 15:42:30 +0530

    fixed bottom tab

--- src/modules/components/Mobile/mobileBottomTab.jsx
+++ src/modules/components/Mobile/mobileBottomTab.jsx
@@ -1,43 +1,22 @@
 import { Box, Tabs, Tab, Avatar } from '@mui/material';
-import ExploreIcon from '../../../assets/ExploreIcon.png'
-import PremiumIcon from '../../../assets/PremiumIcon.png'
-import MessageIcon from '../../../assets/MessageIcon.png'
-import BookingIcon from '../../../assets/BookingIcon.png'
+import ExploreIcon from '../../../assets/ExploreIcon.png';
+import PremiumIcon from '../../../assets/PremiumIcon.png';
+import MessageIcon from '../../../assets/MessageIcon.png';
+import BookingIcon from '../../../assets/BookingIcon.png';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 const MobileBottomTab = () => {
     return (
-        <Box sx={{mt:13, position: 'fixed',overflow:'hidden', bottom: 0 ,  left: 0,right: 0,width: '100%', height:"fit-content",bgcolor: 'white',zIndex: 10,borderTop: '1px solid #ccc'}}>
-            <Tabs aria-label="icon label tabs example" variant="fullWidth" >
-                <Tab
-                    icon={<Avatar src={ExploreIcon} sx={{ width: 29, height: 29 }} />}
-                    label="Explore"
-                    sx={{ fontSize: '0.6rem', minWidth: 'auto', padding: 0.9, pl: 1.3, margin: 0.8, textTransform: 'none' }}
-                />
-
-                <Tab
-                    icon={<Avatar src={PremiumIcon} sx={{ width: 29, height: 29 }} />}
-                    label="Premium"
-                    sx={{ fontSize: '0.6rem', minWidth: 'auto', padding: 0.9, margin: 0.9, textTransform: 'none' }}
-                />
-
-                <Tab
-                    icon={<Avatar src={MessageIcon} sx={{ width: 29, height: 29 }} />}
-                    label="Messages"
-                    sx={{ fontSize: '0.6rem', minWidth: 'auto', padding: 0.9, margin: 0.9 , textTransform: 'none'}}
-                />
-
-                <Tab
-                    icon={<Avatar src={BookingIcon} sx={{ width: 29, height: 29 }} />}
-                    label="Bookings"
-                    sx={{ fontSize: '0.6rem', minWidth: 'auto', padding: 0.9, margin: 0.9, textTransform: 'none'}}
-                />
-
-                <Tab icon={<AccountCircleIcon sx={{ width: 27, height: 27 }} />} label="Profile" sx={{ fontSize: '0.6rem', minWidth: 'auto', padding: 0.9, margin: 0.9, textTransform: 'none'}} />
-
+        <Box sx={{ mt: 13, position: 'fixed', overflow: 'hidden', bottom: 0, left: 0, right: 0, width: '100%', bgcolor: 'white', zIndex: 10, borderTop: '1px solid #ccc' }}>
+            <Tabs aria-label="icon label tabs example" variant="fullWidth" sx={{ minHeight: 45, height: 48, pt:1 }}>
+                <Tab icon={<Avatar src={ExploreIcon} sx={{ width: 22, height: 22 }} />} label="Explore" sx={{ fontSize: '0.55rem', minWidth: 0, px: 0.8, mx: 0.4, py: 0.2, my: 0.2, textTransform: 'none', minHeight: 45, height: 45 }} />
+                <Tab icon={<Avatar src={PremiumIcon} sx={{ width: 22, height: 22 }} />} label="Premium" sx={{ fontSize: '0.55rem', minWidth: 0, px: 0.8, mx: 0.4, py: 0.2, my: 0.2, textTransform: 'none', minHeight: 45, height: 45 }} />
+                <Tab icon={<Avatar src={MessageIcon} sx={{ width: 22, height: 22 }} />} label="Messages" sx={{ fontSize: '0.55rem', minWidth: 0, px: 0.8, mx: 0.4, py: 0.2, my: 0.2, textTransform: 'none', minHeight: 45, height: 45 }} />
+                <Tab icon={<Avatar src={BookingIcon} sx={{ width: 22, height: 22 }} />} label="Bookings" sx={{ fontSize: '0.55rem', minWidth: 0, px: 0.8, mx: 0.4, py: 0.2, my: 0.2, textTransform: 'none', minHeight: 45, height: 45 }} />
+                <Tab icon={<AccountCircleIcon sx={{ width: 22, height: 22 }} />} label="Profile" sx={{ fontSize: '0.55rem', minWidth: 0, px: 0.8, mx: 0.4, py: 0.2, my: 0.2, textTransform: 'none', minHeight: 45, height: 45 }} />
             </Tabs>
         </Box>
-    )
-}
+    );
+};
 
-export default MobileBottomTab
+export default MobileBottomTab;
