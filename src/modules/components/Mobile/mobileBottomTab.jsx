603a891e1158a39908013dc2da3d164b786de278
Akhil-jose-10 <akhiljosetcr@gmail.com>
2025-05-25 16:02:13 +0530

    fixed bottom tab

--- src/modules/components/Mobile/mobileBottomTab.jsx
+++ src/modules/components/Mobile/mobileBottomTab.jsx
@@ -7,7 +7,7 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 const MobileBottomTab = () => {
     return (
-        <Box sx={{ mt: 10, position: 'fixed', overflow: 'hidden', bottom: 0, left: 0, right: 0, width: '100%', bgcolor: 'white', zIndex: 10, borderTop: '1px solid #ccc' }}>
+        <Box sx={{ mt: 9, position: 'fixed', overflow: 'hidden', bottom: 0, left: 0, right: 0, width: '100%', bgcolor: 'white', zIndex: 10, borderTop: '1px solid #ccc' }}>
             <Tabs aria-label="icon label tabs example" variant="fullWidth" sx={{ minHeight: 45, height: 48, pt: 1 }}>
                 <Tab icon={<HomeOutlinedIcon sx={{ width: 25, height: 25 }} />} label="Explore" sx={{ fontSize: '0.55rem', minWidth: 0, px: 0.8, mx: 0.4, py: 0.2, my: 0.2, textTransform: 'none', minHeight: 45, height: 45 }} />
                 <Tab icon={<WorkspacePremiumIcon sx={{ width: 24, height: 24 }} />} label="Premium" sx={{ fontSize: '0.55rem', minWidth: 0, px: 0.8, mx: 0.4, py: 0.2, my: 0.2, textTransform: 'none', minHeight: 45, height: 45 }} />
