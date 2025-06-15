0cbefb8fb911519007edd24d4cf21402673944e3
Akhil-jose-10 <akhiljosetcr@gmail.com>
2025-06-15 22:01:38 +0530

    fixed  workerside request detail  page

--- src/modules/WorkerPage/WorkerRequest/WorkerRequestDetail/WorkerRequestDetail.jsx
+++ src/modules/WorkerPage/WorkerRequest/WorkerRequestDetail/WorkerRequestDetail.jsx
@@ -131,8 +131,8 @@ const WorkerRequestDetail = () => {
                     }}
                 />
                 <Stack direction="column" spacing={1} mb={2} sx={{ position: 'relative', zIndex: 1 }}>
-                    <Work sx={{ fontSize: '3rem', color: '#002366 ' }} />
-                    <Typography sx={{ fontSize: '2rem', color: '#000036 ' }} fontWeight="bold">
+                    <Work sx={{ fontSize: '3rem', color: '#000050' }} />
+                    <Typography sx={{ fontSize: '2rem', color: '#000050' }} fontWeight="bold">
                         Plumbing
                     </Typography>
                 </Stack>
