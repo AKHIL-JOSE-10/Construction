ee83dc5bea9053b2a4f41673c4bcf27b3b10da1d
psabhishek26 <sploitpay.aosp.test@gmail.com>
2025-06-17 08:40:57 +0530

    Apply bottom tab padding only conditionally
    Signed-off-by: psabhishek26 <sploitpay.aosp.test@gmail.com>

--- src/modules/components/Mobile/MobileLayout.jsx
+++ src/modules/components/Mobile/MobileLayout.jsx
@@ -1,9 +1,22 @@
-import { Outlet } from 'react-router-dom';
-import MobileBottomTab from './mobileBottomTab';
+import { Outlet, useLocation } from "react-router-dom";
+import MobileBottomTab from "./mobileBottomTab";
 
 const MobileLayout = () => {
+  const location = useLocation();
+
+  const bottomTabs = [
+    "/home",
+    "/mobile-messages",
+    "/mobile-bookings",
+    "/mobile-profile",
+  ];
+
   return (
-    <div style={{ paddingBottom: '60px' }}>
+    <div
+      style={{
+        paddingBottom: bottomTabs.includes(location.pathname) ? "60px" : "25px",
+      }}
+    >
       <Outlet />
       <MobileBottomTab />
     </div>
