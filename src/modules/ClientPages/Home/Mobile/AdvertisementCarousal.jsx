d429be106a3f8e5c4c2d97d30b2e105a0f79a992
Akhil-jose-10 <akhiljosetcr@gmail.com>
2025-06-17 17:35:28 +0530

    added advertisement

--- src/modules/ClientPages/Home/Mobile/AdvertisementCarousal.jsx
+++ src/modules/ClientPages/Home/Mobile/AdvertisementCarousal.jsx
@@ -24,6 +24,7 @@ const AdvertisementCarousal = () => {
         overflow: 'hidden',
         position: 'relative',
         bgcolor: '#f5f5f5',
+        ml:1
       }}
     >
       {/* Image display */}
@@ -35,11 +36,12 @@ const AdvertisementCarousal = () => {
           alt={`slide-${index}`}
           sx={{
             position: 'absolute',
-            width: '99%',
+            width: '96%',
             height: '100%',
             objectFit: 'contain',
             opacity: currentIndex === index ? 1 : 0,
             transition: 'opacity 1s ease-in-out',
+            
           }}
         />
       ))}
