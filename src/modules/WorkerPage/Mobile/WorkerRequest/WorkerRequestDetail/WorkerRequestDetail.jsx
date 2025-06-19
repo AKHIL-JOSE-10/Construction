9e809b59d2044220607f19b18268037650d8d874
Akhil-jose-10 <akhiljosetcr@gmail.com>
2025-06-19 14:15:35 +0530

    fixed worker request detail page

--- src/modules/WorkerPage/Mobile/WorkerRequest/WorkerRequestDetail/WorkerRequestDetail.jsx
+++ src/modules/WorkerPage/Mobile/WorkerRequest/WorkerRequestDetail/WorkerRequestDetail.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import {
+    Grid,
     Box,
     Typography,
     IconButton,
@@ -48,15 +49,16 @@ const WorkerRequestDetail = () => {
     };
 
     return (
-        <Box sx={{ px: 2, pt:1, pb:2,  maxWidth: 400, mx: 'auto' }}>
+        <Grid >
+            <Box sx={{mt:1, ml:2}}>
             {/* Header Icons */}
             <Stack direction="row" justifyContent="space-between" alignItems="center" mb={2} ml={-1}>
                 <IconButton onClick={() => navigate(-1)}>
                     <ArrowBackIosIcon />
                 </IconButton>
-                <Typography sx={{ fontSize: '1.3rem' }}>Work Detail</Typography>
+                <Typography sx={{ fontSize: '1.3rem', }}>Work Detail</Typography>
                 <IconButton onClick={handleMenuClick}>
-                    <MoreVert sx={{ fontSize: '2rem' }} />
+                    <MoreVert sx={{ fontSize: '2rem', }} />
                 </IconButton>
 
                 {/* Dropdown Menu */}
@@ -89,112 +91,116 @@ const WorkerRequestDetail = () => {
                     </MenuItem>
                 </Menu>
             </Stack>
+            </Box>
+
 
             {/* Title Section */}
-            <Box  >
-                <Stack direction="Row" mb={3} alignItems={'center'} ml={0.2}>
-                    <Work sx={{ fontSize: '3rem', color: 'Black', }} />
-                    <Typography sx={{ fontSize: '1.7rem', color: 'Black', ml: 2, mt: 1 }} fontWeight="bold">
-                        Steel Fabrication
-                    </Typography>
-                    <Box>
+            <Grid sx={{ px: 2, pt: 1, pb: 2, maxWidth: 400, mx: 'auto' }}>
+                <Box  >
+                    <Stack direction="Row" mb={3} alignItems={'center'} ml={0.2}>
+                        <Work sx={{ fontSize: '3rem', color: 'Black', }} />
+                        <Typography sx={{ fontSize: '1.7rem', color: 'Black', ml: 2, mt: 1 }} fontWeight="bold">
+                            Steel Fabrication
+                        </Typography>
+                        <Box>
 
-                    </Box>
-                </Stack>
-            </Box>
-
-            {/* Details */}
-            <Box
-                sx={{
-                    px: 1.5,
-                    py: 2.5,
-                    border: '1px solid #e0e0e0',
-                    boxShadow: '0 2px 8px rgba(0, 0, 0, 0.05)', // subtle shadow
-                    backgroundColor: 'white', // optional for better contrast
-                }}
-            >
-                <Stack direction="row" alignItems="center" spacing={1} mb={3}>
-                    <CalendarToday fontSize="small" />
-                    <Typography sx={{ fontSize: '0.9rem' }}>Sunday, 21 Jan</Typography>
-                </Stack>
-
-                <Stack direction="row" alignItems="center" spacing={1} mb={3}>
-                    <AccessTime fontSize="small" />
-                    <Typography sx={{ fontSize: '0.9rem' }}>09:30 am – 10:30 am</Typography>
-                </Stack>
-
-                <Stack direction="row" alignItems="center" spacing={1} mb={3}>
-
-                    <Avatar src={avatar} sx={{ width: '2rem', height: '2rem' }} />
-                    <Typography sx={{ fontSize: '0.9rem' }}>{client}</Typography>
-                </Stack>
-
-                <Stack direction="row" alignItems="center" spacing={1} mb={3}>
-                    <Phone fontSize="small" />
-                    <Typography sx={{ fontSize: '0.9rem' }}>+91 8828743213</Typography>
-                </Stack>
-
-                <Stack direction="row" alignItems="center" spacing={1}>
-                    <LocationOn fontSize="small" />
-                    <Typography sx={{ fontSize: '0.9rem' }}>{location}</Typography>
-                </Stack>
-            </Box>
-
-            {/* Map Image */}
-            <Box
-                component="img"
-                src={googlemap}
-                alt="Map"
-                sx={{ width: '100%', height: '200px', borderRadius: 1, my: 2 }}
-            />
-
-            {/* Comments */}
-            <Stack direction="row" alignItems="center" spacing={1} mb={0.5}>
-                <Comment fontSize="small" />
-                <Typography fontWeight={600}>Comments</Typography>
-            </Stack>
-            <Typography variant="body2" color="text.secondary" mb={2}>
-                Jan 21 you will be in Tokyo. Book a flight ticket. This is in clash with other meeting
-            </Typography>
-
-            <Box>
-                <Divider sx={{ mb: 2 }} />
-
-                {/* Confirm & Cancel Buttons */}
-                {isFromConfirmation && (
-                    <Stack direction="row" spacing={2} justifyContent="center">
-                        <Button
-                            variant="contained"
-                            color="secondary"
-                            startIcon={<CheckCircle />}
-                            sx={{
-                                backgroundColor: '#865B3C',
-                                '&:hover': { backgroundColor: '#6d4831' },
-                                textTransform: 'none',
-                                px: 4,
-                                borderRadius: 5,
-                            }}
-                        >
-                            Confirm
-                        </Button>
-                        <Button
-                            variant="outlined"
-                            startIcon={<Cancel />}
-                            sx={{
-                                borderColor: '#ea6c6c',
-                                color: '#ea6c6c',
-                                textTransform: 'none',
-                                px: 4,
-                                borderRadius: 5,
-                            }}
-                        >
-                            Cancel
-                        </Button>
+                        </Box>
                     </Stack>
-                )}
-            </Box>
+                </Box>
 
-        </Box>
+                {/* Details */}
+                <Box
+                    sx={{
+                        px: 1.5,
+                        py: 2.5,
+                        border: '1px solid #e0e0e0',
+                        boxShadow: '0 2px 8px rgba(0, 0, 0, 0.05)', // subtle shadow
+                        backgroundColor: 'white', // optional for better contrast
+                    }}
+                >
+                    <Stack direction="row" alignItems="center" spacing={1} mb={3}>
+                        <CalendarToday fontSize="small" />
+                        <Typography sx={{ fontSize: '0.9rem' }}>Sunday, 21 Jan</Typography>
+                    </Stack>
+
+                    <Stack direction="row" alignItems="center" spacing={1} mb={3}>
+                        <AccessTime fontSize="small" />
+                        <Typography sx={{ fontSize: '0.9rem' }}>09:30 am – 10:30 am</Typography>
+                    </Stack>
+
+                    <Stack direction="row" alignItems="center" spacing={1} mb={3}>
+
+                        <Avatar src={avatar} sx={{ width: '2rem', height: '2rem' }} />
+                        <Typography sx={{ fontSize: '0.9rem' }}>{client}</Typography>
+                    </Stack>
+
+                    <Stack direction="row" alignItems="center" spacing={1} mb={3}>
+                        <Phone fontSize="small" />
+                        <Typography sx={{ fontSize: '0.9rem' }}>+91 8828743213</Typography>
+                    </Stack>
+
+                    <Stack direction="row" alignItems="center" spacing={1}>
+                        <LocationOn fontSize="small" />
+                        <Typography sx={{ fontSize: '0.9rem' }}>{location}</Typography>
+                    </Stack>
+                </Box>
+
+                {/* Map Image */}
+                <Box
+                    component="img"
+                    src={googlemap}
+                    alt="Map"
+                    sx={{ width: '100%', height: '200px', borderRadius: 1, my: 2 }}
+                />
+
+                {/* Comments */}
+                <Stack direction="row" alignItems="center" spacing={1} mb={0.5}>
+                    <Comment fontSize="small" />
+                    <Typography fontWeight={600}>Comments</Typography>
+                </Stack>
+                <Typography variant="body2" color="text.secondary" mb={2}>
+                    Jan 21 you will be in Tokyo. Book a flight ticket. This is in clash with other meeting
+                </Typography>
+
+                <Box>
+                    <Divider sx={{ mb: 2 }} />
+
+                    {/* Confirm & Cancel Buttons */}
+                    {isFromConfirmation && (
+                        <Stack direction="row" spacing={2} justifyContent="center">
+                            <Button
+                                variant="contained"
+                                color="secondary"
+                                startIcon={<CheckCircle />}
+                                sx={{
+                                    backgroundColor: '#865B3C',
+                                    '&:hover': { backgroundColor: '#6d4831' },
+                                    textTransform: 'none',
+                                    px: 4,
+                                    borderRadius: 5,
+                                }}
+                            >
+                                Confirm
+                            </Button>
+                            <Button
+                                variant="outlined"
+                                startIcon={<Cancel />}
+                                sx={{
+                                    borderColor: '#ea6c6c',
+                                    color: '#ea6c6c',
+                                    textTransform: 'none',
+                                    px: 4,
+                                    borderRadius: 5,
+                                }}
+                            >
+                                Cancel
+                            </Button>
+                        </Stack>
+                    )}
+                </Box>
+            </Grid>
+
+        </Grid>
     );
 };
 
