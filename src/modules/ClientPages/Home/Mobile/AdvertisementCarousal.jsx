3357ee29713d1603baefa7fdf1cc9f5c93e57935
Akhil-jose-10 <akhiljosetcr@gmail.com>
2025-06-22 17:13:25 +0530

    fixed Sd carousal

--- src/modules/ClientPages/Home/Mobile/AdvertisementCarousal.jsx
+++ src/modules/ClientPages/Home/Mobile/AdvertisementCarousal.jsx
@@ -40,7 +40,7 @@ const AdvertisementCarousal = () => {
           {imageData.map((item, index) => (
             <SwiperSlide key={index}>
               <Card sx={{ borderRadius: 2, mb: 1 }}>
-                <CardMedia component="img" height='200px'  image={item.img} />
+                <CardMedia component="img"  image={item.img} />
               </Card>
             </SwiperSlide>
           ))}
