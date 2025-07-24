8b1a94ebb3cb59147603e18c86952165443739a0
Akhil-jose-10 <akhiljosetcr@gmail.com>
2025-07-24 20:06:10 +0530

    fixed home screen service scroll

--- src/modules/ClientPages/Home/Mobile/serviceScrollBar.jsx
+++ src/modules/ClientPages/Home/Mobile/serviceScrollBar.jsx
@@ -113,7 +113,7 @@ const ServiceScrollBar = ({ pages, fakeScrollBarWidthPercent = 50 }) => {
         sx={{
           position: "absolute",
           bottom: 0,
-          left: `${(28 + fakeScrollBarWidthPercent) / 1.8}vw`, // to center it
+          left: `${(28 + fakeScrollBarWidthPercent) / 1.7}vw`, // to center it
           width: `${fakeScrollBarWidthPercent/4}vw`,
           height: "auto",
           overflowX: "auto",
