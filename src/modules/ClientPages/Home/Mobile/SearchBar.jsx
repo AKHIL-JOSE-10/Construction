b8adff13d4448fb5ad8f16de6ab0a10ef315c7cf
Akhil-jose-10 <akhiljosetcr@gmail.com>
2025-07-30 01:22:50 +0530

    fixed home

--- src/modules/ClientPages/Home/Mobile/SearchBar.jsx
+++ src/modules/ClientPages/Home/Mobile/SearchBar.jsx
@@ -21,8 +21,8 @@ const SearchBar = ({ placeholder = 'Search for “Plumbing Services”' }) => {
         alignItems: "center",
         width: "100%",
         cursor: "pointer",
-        border:'0.1px solid #d5e4fcff',
-        boxShadow: "0 0 10px rgba(114, 126, 234, 0.3)", // subtle red shadow
+        border:'0.3px solid #d5e4fcff',
+        boxShadow: "0 0 10px rgba(114, 126, 234, 0.1)", // subtle red shadow
         transition: "box-shadow 0.3s ease, border 0.3s ease",
         "&:hover": {
           boxShadow: "0px 4px 12px rgba(255, 4, 0, 5)", // stronger on hover
