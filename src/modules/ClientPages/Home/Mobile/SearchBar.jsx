485e71596d6eb9b33c3395062c3e3d86bd85fa23
Akhil-jose-10 <akhiljosetcr@gmail.com>
2025-07-30 01:13:38 +0530

    fixed home

--- src/modules/ClientPages/Home/Mobile/SearchBar.jsx
+++ src/modules/ClientPages/Home/Mobile/SearchBar.jsx
@@ -14,14 +14,14 @@ const SearchBar = ({ placeholder = 'Search for “Plumbing Services”' }) => {
     <Box
       sx={{
         mt: 2,
-        bgcolor: "#c9ddfcff", // light red inner shade
+        bgcolor: "#d5e4fcff", // light red inner shade
         borderRadius: 1,
         height: 40,
         display: "flex",
         alignItems: "center",
         width: "100%",
         cursor: "pointer",
-        boxShadow: "0 0 10px rgba(53, 71, 229, 0.7)", // subtle red shadow
+        boxShadow: "0 0 10px rgba(114, 126, 234, 0.7)", // subtle red shadow
         transition: "box-shadow 0.3s ease, border 0.3s ease",
         "&:hover": {
           boxShadow: "0px 4px 12px rgba(255, 4, 0, 5)", // stronger on hover
