06e7c7dbcedd9a18d0dbd343ed828d7a3ece469f
Akhil-jose-10 <akhiljosetcr@gmail.com>
2025-07-30 23:31:05 +0530

    changed design  popular services

--- src/modules/ClientPages/Home/Mobile/mobHome.jsx
+++ src/modules/ClientPages/Home/Mobile/mobHome.jsx
@@ -387,7 +387,7 @@ const MobHome = () => {
         flex: "0 0 auto",
         width: 105,
         height: 105,
-        borderRadius: 2.5,
+        borderRadius: 2,
         overflow: "hidden",
         cursor: "pointer",
         mr: 1,
